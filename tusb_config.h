5e8af1ba7dcf629fbf8bbcb44cd44d35d3d5bda6
Mate Kukri <mate.kukri@canonical.com>
2026-04-21 17:53:28 +0100

    Enlarge CDC FIFO and EP buffers to 8 KiB
    Default 64 B buffers cap FS CDC throughput at ~64 KiB/s per frame. Both
    the FIFO sizes and the EP transfer buffer need to grow; bumping just the
    FIFOs doesn't move the needle because TinyUSB's bulk submissions are
    capped by EP_BUFSIZE. 8 KiB for all three puts us near the FS ceiling.

--- tusb_config.h
+++ tusb_config.h
@@ -107,11 +107,11 @@
 #define CFG_TUD_VENDOR            0
 
 // CDC FIFO size of TX and RX
-#define CFG_TUD_CDC_RX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
-#define CFG_TUD_CDC_TX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
+#define CFG_TUD_CDC_RX_BUFSIZE   1024 * 8
+#define CFG_TUD_CDC_TX_BUFSIZE   1024 * 8
 
 // CDC Endpoint transfer buffer size, more is faster
-#define CFG_TUD_CDC_EP_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
+#define CFG_TUD_CDC_EP_BUFSIZE   1024 * 8
 
 #ifdef __cplusplus
  }
